93aeda99d7d3948a24fbde6f34a9675bea9ebf26
kkrishhh <soham6218@gmail.com>
2025-08-31 20:48:08 +0530

    created endpoints for note app

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 const app = express();
 
-app.get('/api/notes', (req,res)=>{
-    res.status(200).send("hello world")
-});
-
+    app.get('/api/notes', (req,res)=>{
+        res.status(200).send("create your notes")/// An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource.
+    })
+    app.post('/api/notes', (req, res) =>{
+        res.status(201).send("note created succesfully")// we created 4 endpoints
+    })
+    app.put('/api/notes/:id', (req, res) =>{
+        res.status(200).send("note updated succesfully")
+    })
+    app.delete('/api/notes/:id', (req, res) =>{
+        res.status(200).send("note deleted succesfully")
+    })
 app.listen(5001, ()=>{
     console.log("app listening on localhost:5001");
 })
